954d894c4f74a87eea040d3971ca8b7ce10145a4
ianribeiro01 <ianithaloribeiro@gmail.com>
2018-12-18 21:34:45 -0300

    Diretivas: NgDisabled, NgOptions

--- app/listaTelefonicaCtrl.js
+++ app/listaTelefonicaCtrl.js
@@ -10,6 +10,15 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
         {nome: "Ana Clara", telefone: "997361525"}
     ];
 
+    $scope.operadoras = [
+        {nome: "Claro", codigo: 21, categoria: "Celular"},
+        {nome: "Vivo", codigo: 15, categoria: "Celular"},
+        {nome: "Embratel", codigo: 40, categoria: "Fixo"},
+        {nome: "Tim", codigo: 41, categoria: "Celular"},
+        {nome: "Oi", codigo: 31, categoria: "Celular"},
+        {nome: "GVT", codigo: 16, categoria: "Fixo"}
+    ];
+
     $scope.adicionarContato = function(contato){
         //Mantra AngJS: evitar ler o $scope no controller 
         //e tentar ao máximo definir abstração de entidade
